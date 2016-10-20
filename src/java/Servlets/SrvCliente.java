388251f6aec70c35dcd33c06ec0a3a435bd23e9e
Victor Cardoso <victor.eacd@gmail.com>
2016-10-20 13:15:22 -0300

    SrvCliente

--- src/java/Servlets/SrvCliente.java
+++ src/java/Servlets/SrvCliente.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import AcessoDados.AcessoCliente;
+import com.mysql.jdbc.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -49,110 +50,110 @@ public class SrvCliente extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-        
-         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        
-         try {
-            ResultSet res;
-            AcessoCliente cliente = new AcessoCliente();
-            int id = 0;
-            String nome = "";
-            String telefone = "";
-            
-            out.println("<!DOCTYPE html>\n" +
-"<html>\n" +
-"    <head>\n" +
-"        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-"        <title>JSP Page</title>\n" +
-"        \n" +
-"        <!-- Latest compiled and minified CSS -->\n" +
-"        <link rel=\"stylesheet\" href=\"//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" >\n" +
-"        <link rel=\"stylesheet\" href=\"https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css\" >\n" +
-"        <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script>\n" +
-"\n" +
-"        <!-- Optional theme -->\n" +
-"        <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">\n" +
-"\n" +
-"        <!-- Latest compiled and minified JavaScript -->\n" +
-"        <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>\n" +
-"        \n" +
-"    </head>\n" +
-"    <body>\n" +
-"        <nav class=\"navbar navbar-inverse\">\n" +
-" <ul class=\"nav navbar-nav\"> \n" +
-"                <div class=\"navbar-header\">\n" +
-"                    <a class=\"navbar-brand\" href=\"#\"><font><font>SysLuthieria</font></font></a>\n" +
-"                </div>\n" +
-"                <li id=\"home\"><a href=\"http://localhost:8080/SysLuthieria/index.jsp\">Home</a></li>\n" +
-"                <li id=\"cliente\" class=\"dropdown active\">\n" +
-"                    <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">Cliente\n" +
-"                    <span class=\"caret\"></span></a>\n" +
-"                    <ul class=\"dropdown-menu\">\n" +
-"                      <li><a href=\"http://localhost:8080/SysLuthieria/cliente/cadastrar.jsp\">Cadastrar</a></li>\n" +
-"                      <li><a href=\"http://localhost:8080/SysLuthieria/SrvCliente\">Consultar</a></li>\n" +
-"                    </ul>\n" +
-"                </li>\n" +
-"                <li id=\"servico\" class=\"dropdown\">\n" +
-"                    <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">Serviços\n" +
-"                    <span class=\"caret\"></span></a>\n" +
-"                    <ul class=\"dropdown-menu\">\n" +
-"                      <li><a href=\"http://localhost:8080/SysLuthieria/servicos/cadastrar.jsp\">Cadastrar</a></li>\n" +
-"                      <li><a href=\"http://localhost:8080/SysLuthieria/servicos/consultar.jsp\">Consultar</a></li>\n" +
-"                    </ul>\n" +
-"                </li>\n" +
-"                <li id=\"produto\" class=\"dropdown\">\n" +
-"                    <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">Produto\n" +
-"                    <span class=\"caret\"></span></a>\n" +
-"                    <ul class=\"dropdown-menu\">\n" +
-"                      <li><a href=\"http://localhost:8080/SysLuthieria/produto/cadastrar.jsp\">Cadastrar</a></li>\n" +
-"                      <li><a href=\"http://localhost:8080/SysLuthieria/produto/consultar.jsp\">Consultar</a></li>\n" +
-"                    </ul>\n" +
-"                </li>\n" +
-"                <li id=\"instrumento\" class=\"dropdown\">\n" +
-"                    <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\"> Tipo de Instrumento\n" +
-"                    <span class=\"caret\"></span></a>\n" +
-"                    <ul class=\"dropdown-menu\">\n" +
-"                      <li><a href=\"http://localhost:8080/SysLuthieria/instrumento/cadastrar.jsp\">Cadastrar</a></li>\n" +
-"                      <li><a href=\"http://localhost:8080/SysLuthieria/instrumento/consultar.jsp\">Consultar</a></li>\n" +
-"                    </ul>\n" +
-"                </li>\n" +
-"                \n" +
-"            </ul>\n" +
-"         </nav>");
-            
-            out.println("<div style=\"margin-left: 10%; margin-right: 10%;\">"
-                    + "<h1>Consulta de Cliente</h1><hr>");
-            out.println("<table id=\"cliente\" align=center width=100% class=\"table table-striped table-bordered\" >");
-            out.println("<thead><tr><th>ID</th><th>Nome</th><th>Telefone</th></tr></thead>"
-                    + "<tfoot><tr><th>ID</th><th>Nome</th><th>Telefone</th></tr></tfoot><tbody>");
-            res = cliente.Lista();
-            
-                  res = cliente.Lista();
-
-            while (res.next()) {
-                id = res.getInt("id");
-                nome = res.getString("nome");
-                telefone = res.getString("telefone");
-                out.println("<tr><td>" + id
-                        + "</td><td>" + nome 
-                        + "</td><td>" + telefone+"</tr>");
-            }
-            out.println("</tbody></table></div>");
-            
-            out.println("<script src=\"//code.jquery.com/jquery-1.12.3.js\"></script>");
-            out.println("<script src=\"https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js\"></script>");
-            out.println("<script src=\"https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js\"></script>");
-            out.println("<script>\n"
-                        + "$(document).ready(function() { $('#cliente').DataTable();});\n"
-                    + "</script>"
-                    + "</body>");
-            out.println("</html>");
-            
-        } catch (Exception ex) {
-            Logger.getLogger(SrvLogin.class.getName()).log(Level.SEVERE, null, ex);
-        }
+//        processRequest(request, response);
+//        
+//         response.setContentType("text/html;charset=UTF-8");
+//        PrintWriter out = response.getWriter();
+//        
+//         try {
+//            ResultSet res;
+//            AcessoCliente cliente = new AcessoCliente();
+//            int id = 0;
+//            String nome = "";
+//            String telefone = "";
+//            
+//            out.println("<!DOCTYPE html>\n" +
+//"<html>\n" +
+//"    <head>\n" +
+//"        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
+//"        <title>JSP Page</title>\n" +
+//"        \n" +
+//"        <!-- Latest compiled and minified CSS -->\n" +
+//"        <link rel=\"stylesheet\" href=\"//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" >\n" +
+//"        <link rel=\"stylesheet\" href=\"https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css\" >\n" +
+//"        <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script>\n" +
+//"\n" +
+//"        <!-- Optional theme -->\n" +
+//"        <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">\n" +
+//"\n" +
+//"        <!-- Latest compiled and minified JavaScript -->\n" +
+//"        <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>\n" +
+//"        \n" +
+//"    </head>\n" +
+//"    <body>\n" +
+//"        <nav class=\"navbar navbar-inverse\">\n" +
+//" <ul class=\"nav navbar-nav\"> \n" +
+//"                <div class=\"navbar-header\">\n" +
+//"                    <a class=\"navbar-brand\" href=\"#\"><font><font>SysLuthieria</font></font></a>\n" +
+//"                </div>\n" +
+//"                <li id=\"home\"><a href=\"http://localhost:8080/SysLuthieria/index.jsp\">Home</a></li>\n" +
+//"                <li id=\"cliente\" class=\"dropdown active\">\n" +
+//"                    <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">Cliente\n" +
+//"                    <span class=\"caret\"></span></a>\n" +
+//"                    <ul class=\"dropdown-menu\">\n" +
+//"                      <li><a href=\"http://localhost:8080/SysLuthieria/cliente/cadastrar.jsp\">Cadastrar</a></li>\n" +
+//"                      <li><a href=\"http://localhost:8080/SysLuthieria/SrvCliente\">Consultar</a></li>\n" +
+//"                    </ul>\n" +
+//"                </li>\n" +
+//"                <li id=\"servico\" class=\"dropdown\">\n" +
+//"                    <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">Serviços\n" +
+//"                    <span class=\"caret\"></span></a>\n" +
+//"                    <ul class=\"dropdown-menu\">\n" +
+//"                      <li><a href=\"http://localhost:8080/SysLuthieria/servicos/cadastrar.jsp\">Cadastrar</a></li>\n" +
+//"                      <li><a href=\"http://localhost:8080/SysLuthieria/servicos/consultar.jsp\">Consultar</a></li>\n" +
+//"                    </ul>\n" +
+//"                </li>\n" +
+//"                <li id=\"produto\" class=\"dropdown\">\n" +
+//"                    <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">Produto\n" +
+//"                    <span class=\"caret\"></span></a>\n" +
+//"                    <ul class=\"dropdown-menu\">\n" +
+//"                      <li><a href=\"http://localhost:8080/SysLuthieria/produto/cadastrar.jsp\">Cadastrar</a></li>\n" +
+//"                      <li><a href=\"http://localhost:8080/SysLuthieria/produto/consultar.jsp\">Consultar</a></li>\n" +
+//"                    </ul>\n" +
+//"                </li>\n" +
+//"                <li id=\"instrumento\" class=\"dropdown\">\n" +
+//"                    <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\"> Tipo de Instrumento\n" +
+//"                    <span class=\"caret\"></span></a>\n" +
+//"                    <ul class=\"dropdown-menu\">\n" +
+//"                      <li><a href=\"http://localhost:8080/SysLuthieria/instrumento/cadastrar.jsp\">Cadastrar</a></li>\n" +
+//"                      <li><a href=\"http://localhost:8080/SysLuthieria/instrumento/consultar.jsp\">Consultar</a></li>\n" +
+//"                    </ul>\n" +
+//"                </li>\n" +
+//"                \n" +
+//"            </ul>\n" +
+//"         </nav>");
+//            
+//            out.println("<div style=\"margin-left: 10%; margin-right: 10%;\">"
+//                    + "<h1>Consulta de Cliente</h1><hr>");
+//            out.println("<table id=\"cliente\" align=center width=100% class=\"table table-striped table-bordered\" >");
+//            out.println("<thead><tr><th>ID</th><th>Nome</th><th>Telefone</th></tr></thead>"
+//                    + "<tfoot><tr><th>ID</th><th>Nome</th><th>Telefone</th></tr></tfoot><tbody>");
+//            res = cliente.Lista();
+//            
+//                  res = cliente.Lista();
+//
+//            while (res.next()) {
+//                id = res.getInt("id");
+//                nome = res.getString("nome");
+//                telefone = res.getString("telefone");
+//                out.println("<tr><td>" + id
+//                        + "</td><td>" + nome 
+//                        + "</td><td>" + telefone+"</tr>");
+//            }
+//            out.println("</tbody></table></div>");
+//            
+//            out.println("<script src=\"//code.jquery.com/jquery-1.12.3.js\"></script>");
+//            out.println("<script src=\"https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js\"></script>");
+//            out.println("<script src=\"https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js\"></script>");
+//            out.println("<script>\n"
+//                        + "$(document).ready(function() { $('#cliente').DataTable();});\n"
+//                    + "</script>"
+//                    + "</body>");
+//            out.println("</html>");
+//            
+//        } catch (Exception ex) {
+//            Logger.getLogger(SrvLogin.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
 
     /**
@@ -166,46 +167,28 @@ public class SrvCliente extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         
-         try {
-            ResultSet res;
-            AcessoCliente cliente = new AcessoCliente();
-            int id = 0;
-            String nome = "";
-            String telefone = "";
-            
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SrvLogin</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SrvLogin at " + request.getContextPath() + "</h1>");
-            out.println("<table align=center width=289 border=1 class=datos_form>");
-            out.println("<tr class=titulo_tabla><td colspan=3>LISTADO DE CLIENTE - POST</td></tr> ");
-            out.println("<tr><td>ID</td><td>Nome</td><td>Telefone</td></tr>");
-            res = cliente.Lista();
-            
-                  res = cliente.Lista();
+        String nome, telefone;
+      
+        
+        nome = request.getParameter("nome");
+        telefone = request.getParameter("telefone");
+      
+        
+        AcessoCliente con = new AcessoCliente();
+        
+        try{
 
-            while (res.next()) {
-                id = res.getInt("id");
-                nome = res.getString("nome");
-                telefone = res.getString("telefone");
-                out.println("<tr><td>" + id
-                        + "</td><td>" + nome 
-                        + "</td><td>" + telefone+"</tr>");
-            }
-            out.println("</table>");
+            String sql = "INSERT INTO CLIENTE (nome, telefone) VALUES('"+nome+"', '"+telefone+"')";
+            PreparedStatement stmt = (PreparedStatement) con.Conectar().prepareStatement(sql);
             
-            out.println("</body>");
-            out.println("</html>");
+            stmt.execute();
             
-        } catch (Exception ex) {
-            Logger.getLogger(SrvLogin.class.getName()).log(Level.SEVERE, null, ex);
+            response.sendRedirect("cliente/consultar.jsp"); 
+            
+        }catch (Exception e){
+            out.println(e);
         }
     }
 
