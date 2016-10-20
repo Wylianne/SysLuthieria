6f7d1793dc1a3b76327f8b62abadd2d498438a53
Victor Cardoso <victor.eacd@gmail.com>
2016-10-19 21:59:44 -0300

    Acessa produtos java

---
+++ src/java/AcessoDados/AcessoProduto.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AcessoDados;
+
+
+import AcessoDados.Conexao;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author victorcardoso
+ */
+public class AcessoProduto extends Conexao{
+    private int id;
+    private String nome;
+    private String marca;
+    private double valor;
+    private ResultSet resultado;
+    
+    public AcessoProduto(){
+        Conectar();
+    }
+    //BUSCA POR TODOS
+    public ResultSet Lista()throws Exception{
+        try{
+        
+            getStmt();
+            resultado = stmt.executeQuery("SELECT * FROM produto");
+            return resultado;
+        }catch (Exception ex){
+            System.out.println("SQLException: "+ ex.getMessage());
+            return null;
+        }
+    }
+    
+   
+    public ResultSet BuscaExixtente(int Codigo) throws Exception{
+
+        try{
+            getStmt();
+            resultado = stmt.executeQuery("SELECT * FROM produto WHERE ID="+Codigo);
+            return resultado;
+        }catch (Exception ex){
+            System.out.println("SQLException: "+ex.getMessage());
+            return null;
+        }
+    }
+}
